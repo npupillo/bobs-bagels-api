00ab386038ca1ef1d2f62f53249a1b9bc13480f2
idelairre <idelairre@gmail.com>
2015-03-12 14:34:47 -0400

    adds serializers for all models and cleans up migrations.

---
+++ db/migrate/20150312183251_add_references_to_order_status.rb
@@ -0,0 +1,6 @@
+class AddReferencesToOrderStatus < ActiveRecord::Migration
+  def change
+    add_reference :order_statuses, :order, index: true
+    add_foreign_key :order_statuses, :orders
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312180316) do
+ActiveRecord::Schema.define(version: 20150312183251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20150312180316) do
 
   add_index "order_items", ["order_id"], name: "index_order_items_on_order_id", using: :btree
   add_index "order_items", ["product_id"], name: "index_order_items_on_product_id", using: :btree
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "order_id"
+  end
+
+  add_index "order_statuses", ["order_id"], name: "index_order_statuses_on_order_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
     t.decimal  "taxes"
@@ -48,4 +57,5 @@ ActiveRecord::Schema.define(version: 20150312180316) do
 
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
+  add_foreign_key "order_statuses", "orders"
 end
