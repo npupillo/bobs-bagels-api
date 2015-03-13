93b730282a5611cce5b2a1c063b7f91248598271
idelairre <idelairre@gmail.com>
2015-03-13 19:45:47 -0400

    modifies the charges controller, routes and schema to process charge requests from the front end.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,35 +1,59 @@
 require 'stripe'
 
 class ChargesController < ApplicationController
+
 	def index
-		token =	Stripe::Token.create(
-			:bank_account => {
-			:country => "US",
-			:routing_number => "110000000",
-			:account_number => "000123456789",
-			},
-		)
+		@charges = Charge.all
+		render json: @charges
+	end
+	
+	def make_charge
+		@charge = Charge.new(charge_params)
 
-		customer = Stripe::Customer.retrieve("cus_5rOOrVnSCnMESX")
+		if @charge.save
+			render json: { charge: @charge }
+		else
+			render json: {message: 'failed', status: 500}
+        end
 
-		card = Stripe::Token.create(
-			:card => {
-			:number => "4242424242424242",
-			:exp_month => 3,
-			:exp_year => 2016,
-			:cvc => "314"
-		  },
-		)
+#		customer = Stripe::Customer.create(
+#			:email => 'example@stripe.com',
+#			:card  => params[:stripeToken]
+#		  )
+#		
+		customer = Stripe::Customer.retrieve(@charge.customer_id)
 
-		@amount = 500
+
+#		card = Stripe::Token.create(
+#			:card => {
+#			:number => "4242424242424242",
+#			:exp_month => 3,
+#			:exp_year => 2016,
+#			:cvc => "314"
+#		  },
+#		)
 
 		charge = Stripe::Charge.create(
 			:customer => customer.id,
-			:amount => @amount,
+			:amount => @charge.amount,
 			:description => 'Rails Stripe customer',
 			:currency => 'usd'
 			)
-
-		render json: { :charge => charge, :card => card, :customer => customer, :token => token }
-	end
+		
+		end
+		
+		private
+		
+		def charge_params
+			params.require(:charge).permit(:charge, :amount, :token, :customer_id)
+		end
 end
+
+
+#      def create
+#        @user = User.new(user_params)
+#        if @user.save
+#          render json: {token: @user.token}
+#        else
+#          render json: {message: 'failed', status: 500}
+#        end
