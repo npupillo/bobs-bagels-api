5a58d0697d7648b5640ea6bb72adfd9ebb11abfd
Max Blaushild <blaushmild@gmail.com>
2015-03-14 16:48:22 -0400

    fixes a backwards relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-	belongs_to :order_item
+	has_many :order_items
 	has_many :ingredients_in_product, foreign_key: "product_id"
   has_many :ingredients, through: :ingredients_in_product
 end
